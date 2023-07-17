20276b2b44e68d61c1cd4fb70250a2780fa858c7
Malik Murray <130183731+murraymalik2@users.noreply.github.com>
2023-07-17 12:21:07 -0400

    Add files via upload

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var michaelsAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+           
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            
+            michaelsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {michaelsAccount.GetBalance()}");
         }
     }
 }
